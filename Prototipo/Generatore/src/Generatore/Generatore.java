147780dbe901b7110a1beca4efd4daededc21925
Francesco <38076434+FrancescoPennacchia@users.noreply.github.com>
2019-02-20 16:47:40 +0100

    Up

--- Prototipo/Generatore/src/Generatore/Generatore.java
+++ Prototipo/Generatore/src/Generatore/Generatore.java
@@ -20,6 +20,7 @@ public class Generatore {
         LinkedList<Edificio> listEdificio = null;
         LinkedList<Distretto> listDistretto = null;
         LinkedList<Area> listArea = null;
+        LinkedList<Sensore> listSensore = null;
 
 
         /*Controllo se ci sono sensori*/
@@ -28,11 +29,9 @@ public class Generatore {
             int numero_distretti = 5;
             int numer_edifici = 20;
             int numero_aree = 10;
-            int numero_sensori = 5;
 
 
             System.out.println("Non ci sono sensori installati");
-            System.out.println("Procedo a generare i sensori");
             System.out.println("\n");
 
             // generazione admin installazione
@@ -59,11 +58,11 @@ public class Generatore {
             System.out.println("--- GENERATE LE AREE ---");
 
 
+            System.out.println("\n");
+            System.out.println("--- GENERAZIONE SENSORI ---");
             long startTime = System.currentTimeMillis(); // Timer Per il tempo impiegato nella generazione dei sensori
             //Chiama il generatore di sensori
-            generaSensori(listDistretto,listEdificio,listArea, numero_sensori);      // Numero di sensori per area passato come parametro
-
-
+            generaSensori(listArea);      // Genera i sensori per ogni area (Ne inserisce 1 per tipo)
 
             long stopTime = System.currentTimeMillis();
             long elapsedTime = stopTime - startTime;
@@ -71,64 +70,70 @@ public class Generatore {
             System.out.println("---SONO STATI GENERATI: " + cs.numeroSensori() + " SENSORI --");
             System.out.println("Tempo impiegato per la generazione dei sensori: " + elapsedTime + "ms");
 
+            startTime = System.currentTimeMillis();
+            // Generazione segnali per ogni sensore
+            listSensore = cs.selectSensori();
+            System.out.println("\n");
+            System.out.println("--- INSERIMENTO PRIMI SEGNALI ---");
+            generaSegnale(listSensore, 2);                      // Numero segnali per ogni sensore
 
+            stopTime = System.currentTimeMillis();
+            elapsedTime = stopTime - startTime;
 
-
+            System.out.println("\n");
+            System.out.println("--- SEGNALI GENERATI IN "+ elapsedTime +" ---");
 
         }
 
 
-        System.out.println("Ci sono già " + cs.numeroSensori() + " Sensori installati nel sistema. Inizio procedura di aggiornamento");
+        System.out.println("\n");
+        System.out.println("Ci sono: " + cs.numeroSensori() + " Sensori installati nel sistema. Inizio procedura di aggiornamento");
         /* Procedura di aggiornamento */
+        while(true){
+
+        }
 
     }
 
 
     /* Genera una lista di sensori, il numero di sesnori gli viene passato come parametro come anche il tipo e il massimale */
-    static void generaSensori(LinkedList<Distretto> distretto, LinkedList<Edificio> edificio, LinkedList<Area> area, int numero_sensori){
-        Random random = new Random();
+    static void generaSensori(LinkedList<Area> area){
 
         Database cs = new Database();
+        Date time = null;
         int massimale = 0;
 
+        for(Area a: area){
 
-        for(Distretto d: distretto){
-            for(Edificio e: edificio){
-                for(Area a: area){
+            /* Sensori di temperatura */
+            massimale = 30;
 
-                    /* Sensori di temperatura */
-                    massimale = 30;
-                    for(int i = 1; i <= numero_sensori/4; i++){
-                        Date time = getDateTime();                                          // Prende la data attuale
-                        cs.insertSensore("Temperatura", massimale,60, time, a.getID(),1);   //Inserisce il sensore.
-                    }
+            time = getDateTime();                                          // Prende la data attuale
+            cs.insertSensore("Temperatura", massimale,60, time, a.getID(),1);   //Inserisce il sensore.
 
-                    /* Sensori di pressione */
-                    massimale = 110;
-                    for(int i = 1; i <= numero_sensori/4; i++){
-                        Date time = getDateTime();                                          // Prende la data attuale
-                        cs.insertSensore("Pressione", massimale,60, time, a.getID(),1);  //Inserisce il sensore.
-                    }
 
-                    /* Sensori di luminosità */
-                    massimale = 50;
-                    for(int i = 1; i <= numero_sensori/4; i++){
-                        Date time = getDateTime();                                          // Prende la data attuale
-                        cs.insertSensore("Luminosità ", massimale,60, time, a.getID(),1);
-                    }
+            /* Sensori di pressione */
+            massimale = 110;
 
-                    /* Sensori di umidità */
-                    massimale = 100;
+            time = getDateTime();                                          // Prende la data attuale
+            cs.insertSensore("Pressione", massimale,60, time, a.getID(),1);  //Inserisce il sensore.
 
-                    for(int i = 1; i <= numero_sensori/4; i++){
-                        Date time = getDateTime();
-                        cs.insertSensore("Umidità ", massimale,60, time, a.getID(),1);
-                    }
 
-                }
-            }
+            /* Sensori di luminosità */
+            massimale = 50;
+
+            time = getDateTime();                                          // Prende la data attuale
+            cs.insertSensore("Luminosità", massimale,60, time, a.getID(),1);
+
+            /* Sensori di umidità */
+            massimale = 100;
+
+
+            time = getDateTime();
+            cs.insertSensore("Umidità", massimale,60, time, a.getID(),1);
+
+
         }
-
     }
 
 
@@ -168,7 +173,6 @@ public class Generatore {
     /* Metodo che prende la data attuale */
     private static Date getDateTime() {
         Date dNow = new Date( );
-        SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
 
         return dNow;
     }
@@ -179,7 +183,34 @@ public class Generatore {
         cs.insertGestore(1, "admin", "admin", null, 1);
     }
 
+    /* Genera Segnale */
+    static void generaSegnale(LinkedList<Sensore> list, int numero_segnali){
+        Database cs = new Database();
+        Random rand = new Random();
+        Date time;
 
-
-
+        for(Sensore s: list){
+            if(s.getTipo().equals("Temperatura")){
+                for(int i = 1; i <= numero_segnali; i++){
+                    time = getDateTime();
+                    cs.insertSegnali(s.getID(), time, rand.nextInt(30),0);
+                }
+            }else if(s.getTipo().equals("Pressione")){
+                for(int i = 1; i <= numero_segnali; i++){
+                    time = getDateTime();
+                    cs.insertSegnali(s.getID(), time, rand.nextInt(110),0);
+                }
+            }else if(s.getTipo().equals("Luminosità")) {
+                for (int i = 1; i <= numero_segnali; i++) {
+                    time = getDateTime();
+                    cs.insertSegnali(s.getID(), time, rand.nextInt(50), 0);
+                }
+            } else if(s.getTipo().equals("Umidità")) {
+                for(int i = 1; i <= numero_segnali; i++){
+                    time = getDateTime();
+                    cs.insertSegnali(s.getID(), time, rand.nextInt(100),0);
+                }
+            }
+        }
+    }
 }
--- Prototipo/Generatore/src/Server/Database.java
+++ Prototipo/Generatore/src/Server/Database.java
@@ -14,14 +14,13 @@ import java.util.LinkedList;
 public class Database {
     int numero_dis = 10; // NumeroDistretti
 
+    Connection dbConnection = ConnectionDB.Connect();
 
     /* Controlla se ci sono sensori*/
     public boolean emptySensore(){
 
-        Connection dbConnection = ConnectionDB.Connect();
 
         try {
-
             Statement stmt = dbConnection.createStatement() ;
             String query = "select count(*) AS totale from Sensore;" ;
             ResultSet rs = stmt.executeQuery(query) ;
@@ -29,16 +28,13 @@ public class Database {
             rs.next();
 
             if(rs.getInt("totale") == 0){
-                dbConnection.close();
+
                 return true;
             }
 
-
-            dbConnection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
 
         return false;
     }
@@ -46,7 +42,6 @@ public class Database {
     /* Restituisce il numero di sensori */
     public int numeroSensori(){
 
-        Connection dbConnection = ConnectionDB.Connect();
 
         try {
 
@@ -72,7 +67,6 @@ public class Database {
     public LinkedList<Area> selectArea(){
 
         LinkedList<Area> list = new LinkedList<Area>();
-        Connection dbConnection = ConnectionDB.Connect();
         int id = 0;
         int alert = 0;
 
@@ -90,7 +84,7 @@ public class Database {
                 list.add(a);
 
             }
-            dbConnection.close();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -103,7 +97,6 @@ public class Database {
     /* Ritorna la lista in base alla tipolgoia Edificio*/
     public LinkedList<Edificio> selectEdifici(){
         LinkedList<Edificio> list = new LinkedList<Edificio>();
-        Connection dbConnection = ConnectionDB.Connect();
         int id = 0;
         int alert = 0;
 
@@ -119,7 +112,7 @@ public class Database {
                 Edificio e = new Edificio(id, alert, numero_dis);
                 list.add(e);
             }
-            dbConnection.close();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -131,7 +124,6 @@ public class Database {
     /* Ritorna la lista in base alla tipolgoia Distretto*/
     public LinkedList<Distretto> selectDistretti(){
         LinkedList<Distretto> list = new LinkedList<Distretto>();
-        Connection dbConnection = ConnectionDB.Connect();
         int id = 0;
         int alert = 0;
 
@@ -148,7 +140,6 @@ public class Database {
                 list.add(d);
             }
 
-            dbConnection.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -160,7 +151,6 @@ public class Database {
 
     /* Inserimento Edificio*/
     public void insertEdifici(String nome, int idDistretto, int alert){
-        Connection dbConnection = ConnectionDB.Connect();
 
         String insertTableSQL = "INSERT INTO edificio" + "(nome, idDistretto, alert) VALUES" + "(?,?,?)";
         try{
@@ -170,7 +160,7 @@ public class Database {
             pre.setInt(3,alert);
             pre.executeUpdate();
             pre.close();
-            dbConnection.close();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -179,7 +169,7 @@ public class Database {
 
     /* Inserimento Distretto */
     public void insertDistretto(String nome, int alert){
-        Connection dbConnection = ConnectionDB.Connect();
+
 
         String insertTableSQL = "INSERT INTO distretto" + "(nome, alert) VALUES" + "(?,?)";
         try{
@@ -188,7 +178,7 @@ public class Database {
             pre.setInt(2, alert);
             pre.executeUpdate();
 
-            dbConnection.close();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -197,7 +187,7 @@ public class Database {
 
     /* inserimento Area */
     public void insertArea(String nome, int Edificio, int alert){
-        Connection dbConnection = ConnectionDB.Connect();
+
 
         String insertTableSQL = "INSERT INTO area" + "(nome, alert, idEdificio) VALUES" + "(?,?,?)";
         try{
@@ -208,7 +198,6 @@ public class Database {
             pre.executeUpdate();
             pre.close();
 
-            dbConnection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -217,7 +206,7 @@ public class Database {
 
     /*Inserimento gestore */
     public void insertGestore(int id,String username, String pass, String tipo, int admin){
-        Connection dbConnection = ConnectionDB.Connect();
+
         String insertTableSQL = "INSERT INTO utente" + "(idUtente, username, password, tipologia, admin) VALUES" + "(?,?,?,?,?)";
         try{
             PreparedStatement pre = dbConnection.prepareStatement(insertTableSQL);
@@ -228,7 +217,7 @@ public class Database {
             pre.setInt(5, admin);
             pre.executeUpdate();
             pre.close();
-            dbConnection.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -238,7 +227,6 @@ public class Database {
 
     /* Inserimento sensore DB e primo segnale*/
     public void insertSensore(String tipo, int massimale, int frequenzaInvio, java.util.Date ultimoinvio,int area, int gestore){
-        Connection dbConnection = ConnectionDB.Connect();
 
 
         String insertTableSQL = "INSERT INTO sensore" + "(tipo, massimale, frequenzaInvio, ultimoInvio, area, gestore) VALUES" + "(?,?,?,?,?,?)";
@@ -254,7 +242,6 @@ public class Database {
 
             pre.executeUpdate();
             pre.close();
-            dbConnection.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -262,12 +249,31 @@ public class Database {
 
     }
 
+    /* InserimentoSegnali */
+    public void insertSegnali(int idSensore, java.util.Date ultimoinvio, float variabile_ambientale, int alert){
+        String insertTableSQL = "INSERT INTO segnale" + "(sensore, timestamp, variabileAmbientale, alert) VALUES" + "(?,?,?,?)";
 
+        try{
+            PreparedStatement pre = dbConnection.prepareStatement(insertTableSQL);
+            pre.setInt(1, idSensore);
+            pre.setDate(2, convertJavaDateToSqlDate(ultimoinvio));
+            pre.setFloat(3, variabile_ambientale);
+            pre.setInt(4, alert);
+            pre.executeUpdate();
+            pre.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-    /* Ritorna lista Sensori */
-    public LinkedList<Sensore> listaSensori(){
+    /* restituisce la lista dei sensori */
+    public LinkedList<Sensore> selectSensori(){
+
         LinkedList<Sensore> list = new LinkedList<Sensore>();
-        Connection dbConnection = ConnectionDB.Connect();
+        int id = 0;
+        String tipo = "";
+        float massimale = 0;
+        int frequenza = 0;
 
         try {
             Statement stmt = dbConnection.createStatement() ;
@@ -275,16 +281,64 @@ public class Database {
             ResultSet rs = stmt.executeQuery(query) ;
 
             while(rs.next()){
-                //Sensore s = new Sensore(id,0, "Luminosità", valore_ambientale, massimale, 60,time);
-                //list.add(s);
+                id = rs.getInt("idSensore");
+                tipo = rs.getString("tipo");
+                massimale = rs.getFloat("massimale");
+                frequenza = rs.getInt("frequenzaInvio");
+                Date data = rs.getDate("ultimoInvio");
+
+                Sensore s = new Sensore(id,0, tipo, massimale, frequenza, data);
+                list.add(s);
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-
         return list;
+
+    }
+
+    /* Ritorna lista Segnali */
+    public LinkedList<Segnale> selectSegnali(){
+        LinkedList<Segnale> list = new LinkedList<Segnale>();
+        int idSegnale = 0;
+        int idSensore =0;
+        Date date;
+        float value = 0;
+        int alert = 0;
+
+        try {
+            Statement stmt = dbConnection.createStatement() ;
+            String query = "select * from segnale";
+            ResultSet rs = stmt.executeQuery(query) ;
+
+            while(rs.next()){
+                idSegnale = rs.getInt("idSegnale");
+                idSensore = rs.getInt("sensore");
+                date = rs.getDate("timestamp");
+                value = rs.getFloat("variabileAmbientale");
+                alert = rs.getInt("alert");
+
+                Segnale s = new Segnale(idSegnale,idSensore,date,value,alert);
+
+                list.add(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+
+    /* Chisuura connessione */
+    public void closeConnection(){
+        try {
+            dbConnection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /*Metodo per convertire il tipo Date di Java al tipo Date mysql*/
